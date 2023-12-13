29bc060d7b9db390b8b3d793065a575a10137a10
Israel Onoja <Israelonoja21@gmail.com>
2023-12-13 01:45:48 +0100

    create a new note function

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import Editor from "../components/Editor";
+import Sidebar from "../components/Sidebar";
+import { nanoid } from "nanoid";
 
 
 export default function App(){
+
+  const [notes, setNotes] = React.useState([])
+  const [currentNoteId, setCurrentNoteId] = React.useState(
+    (notes[0] && notes[0].id) || ''
+  )
+
+  //create a new note
+
+  function createNote(){
+    const newNote = {
+      id: nanoid(),
+      body: "# Type your markdown note's title here"
+    }
+    setNotes(prevNote => [newNote, ...prevNote])
+    setCurrentNoteId(newNote.id)
+  }
+
+
   return (
     <h1>Hello World</h1>
   )
