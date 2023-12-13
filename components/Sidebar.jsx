d728d818e975a4a4cb158a46084d4fbc168bdd78
Israel Onoja <Israelonoja21@gmail.com>
2023-12-13 02:51:07 +0100

    sidebar ui and functionality

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
 
-export default function Sidebar() {
+export default function Sidebar(props) {
+  const noteElements = props.notes.map((note, index) => {
+    <div key={note.id}>
+      <div
+        className={`title ${
+          note.id === props.currentNote.id ? "selected-note" : ""
+        }`}
+        onClick={() => props.setCurrentNote(note.id)}
+      >
+        <h4 className="text-snippet">Note {index + 1}</h4>
+      </div>
+    </div>;
+  });
   return (
-    <div>
-      
-    </div>
-  )
+    <section className="pane sidebar">
+      <div className="sidebar--header">
+        <h3>Notes</h3>
+        <button className="new-note" onClick={props.newNote}>
+          +
+        </button>
+      </div>
+      {noteElements}
+    </section>
+  );
 }
